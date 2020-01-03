14e12207897d80c190986376b7c65923d2f7990c
KimiDing <dingxing0113@163.com>
2020-01-03 23:20:33 +0800

    UnlockMusicDotNet first version

---
+++ UnlockMusicDotNet/Mask.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace UnlockMusicDotNet
+{
+    //用于mflac加密格式
+    public class Mask
+    {
+        public int index { get; set; }
+        public int mask_index { get; set; }
+        public byte[] mask { get; set; }
+        public int[] flac_Header { get; set; }
+
+        public Mask()
+        {
+            this.index = -1;
+            this.mask_index = -1;
+            this.mask = new byte[128];
+            this.flac_Header = new int[] { 0x66, 0x4C, 0x61, 0x43, 0x00 };
+        }
+
+        public bool detectMask(byte[] data)
+        {
+            int search_len = data.Length - 256;
+            byte[] mask = new byte[128];
+
+            for (int block_idx = 0; block_idx < search_len; block_idx += 128)
+            {
+                bool flag = true;
+                //mask = data.slice(block_idx, block_idx + 128);
+                //let next_mask = data.slice(block_idx + 128, block_idx + 256);
+                Array.Copy(data, block_idx, mask, 0, 128);
+
+                byte[] next_mask = new byte[128];
+                Array.Copy(data, block_idx + 128, next_mask, 0, 128);
+                for (int idx = 0; idx < 128; idx++)
+                {
+                    if (mask[idx] != next_mask[idx])
+                    {
+                        flag = false;
+                        break;
+                    }
+                }
+                if (!flag) continue;
+
+
+                for (int test_idx = 0; test_idx < this.flac_Header.Length; test_idx++)
+                {
+                    int p = data[test_idx] ^ mask[test_idx];
+                    if (p != this.flac_Header[test_idx])
+                    {
+                        flag = false;
+                        break;
+                    }
+                }
+                if (!flag) continue;
+                this.mask = mask;
+                return true;
+            }
+            return false;
+        }
+
+        public byte nextMask()
+        {
+            this.index++;
+            this.mask_index++;
+            if (this.index == 0x8000 || (this.index > 0x8000 && (this.index + 1) % 0x8000 == 0))
+            {
+                this.index++;
+                this.mask_index++;
+            }
+            if (this.mask_index >= 128)
+            {
+                this.mask_index -= 128;
+            }
+            return Convert.ToByte(this.mask[this.mask_index]);
+        }
+    }
+}
---
+++ UnlockMusicDotNet/Program.cs
@@ -0,0 +1,159 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace UnlockMusicDotNet
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string currentPath = Directory.GetCurrentDirectory();
+            var files = Directory.GetFiles(currentPath, ".", SearchOption.AllDirectories).Where(s => s.ToLower().EndsWith(".qmc3") || s.ToLower().EndsWith(".qmcflac") || s.ToLower().EndsWith(".qmcogg") || s.ToLower().EndsWith(".qmc0") || s.ToLower().EndsWith(".mflac"));
+
+            if (files.Count() > 0)
+            {
+                using (ManualResetEvent manualResetEvent = new ManualResetEvent(false))
+                {
+                    int maxThreadCount = files.Count();
+                    foreach (var file in files)
+                    {
+                        if (Path.GetExtension(file).Equals(".mflac", StringComparison.OrdinalIgnoreCase))
+                        {
+                            ThreadPool.QueueUserWorkItem(t =>
+                            {
+                                ProcessMflac(file);
+                                if (Interlocked.Decrement(ref maxThreadCount) == 0)
+                                    manualResetEvent.Set();
+                            });
+                        }
+                        else
+                        {
+                            ThreadPool.QueueUserWorkItem(t =>
+                            {
+                                ProcessAncient(file);
+                                if (Interlocked.Decrement(ref maxThreadCount) == 0)
+                                    manualResetEvent.Set();
+                            });
+                        }
+                    }
+
+                    manualResetEvent.WaitOne();
+                }
+                Console.WriteLine("Finish");
+            }
+            else
+            {
+                Console.WriteLine("Can Not find QQMusic file in directory");
+            }
+
+            Console.WriteLine("UnlockMusicDotNet override by KimiDing");
+            Console.ReadKey();
+        }
+
+        private static void ProcessAncient(object filePath)
+        {
+            using (FileStream fsreadFile = new FileStream((string)filePath, FileMode.Open))
+            {
+                string outputPath = Path.GetFileNameWithoutExtension((string)filePath) + ProcessExtension((string)filePath);
+                if (File.Exists(outputPath))
+                {
+                    Console.WriteLine(outputPath + " is exist skip!!!");
+                }
+                else
+                {
+                    using (FileStream fswriteFile = new FileStream(outputPath, FileMode.CreateNew))
+                    {
+                        byte[] buffer = new byte[8192];
+                        int readSize = 0;
+                        int offset = 0;
+                        Seed seed = new Seed();
+                        do
+                        {
+                            readSize = fsreadFile.Read(buffer, 0, 8192);
+                            for (int i = 0; i < 8192; i++)
+                            {
+                                buffer[i] = Convert.ToByte(seed.next_mask() ^ buffer[i]);
+                            }
+                            offset += readSize;
+                            fswriteFile.Write(buffer, 0, buffer.Length);
+                        } while (readSize > 0);
+
+                        Console.WriteLine(filePath + " Done!!!");
+                    }
+                }
+
+            }
+
+        }
+
+        private static void ProcessMflac(object filePath)
+        {
+            using (FileStream fsreadFile = new FileStream((string)filePath, FileMode.Open))
+            {
+                string outputPath = Path.GetFileNameWithoutExtension((string)filePath) + ProcessExtension((string)filePath);
+                if (File.Exists(outputPath))
+                {
+                    Console.WriteLine(outputPath + " is exist skip!!!");
+                }
+                else
+                {
+                    using (FileStream fswriteFile = new FileStream(outputPath, FileMode.CreateNew))
+                    {
+
+                        byte[] tempBuffer = new byte[fsreadFile.Length - 0x170];
+                        byte[] buffer = new byte[8192];
+                        int readSize = 0;
+                        int offset = 0;
+
+                        Mask mask = new Mask();
+                        fsreadFile.Read(tempBuffer, 0, (int)(fsreadFile.Length - 0x170));
+                        if (mask.detectMask(tempBuffer))
+                        {
+                            fsreadFile.Position = 0;
+                            do
+                            {
+                                readSize = fsreadFile.Read(buffer, 0, 8192);
+                                for (int i = 0; i < 8192; i++)
+                                {
+                                    buffer[i] = Convert.ToByte(mask.nextMask() ^ buffer[i]);
+                                }
+                                offset += readSize;
+                                fswriteFile.Write(buffer, 0, buffer.Length);
+                            } while (readSize > 0);
+
+                            Console.WriteLine(filePath + " Done!!!");
+                        }
+                        else
+                        {
+                            Console.WriteLine(filePath + " Not Support!!!");
+                        }
+                    }
+                }
+            }
+        }
+
+        private static string ProcessExtension(string filePath)
+        {
+            string ext = Path.GetExtension(filePath);
+            switch (ext)
+            {
+                case ".qmcflac":
+                    return ".flac";
+                case ".qmc3":
+                    return ".mp3";
+                case ".qmc0":
+                    return ".mp3";
+                case ".qmcogg":
+                    return ".ogg";
+                case ".mflac":
+                    return ".flac";
+                default:
+                    return "";
+            }
+        }
+    }
+
+}
